fd4d5d908a4f62076edbb354e14e7625980fe79f
Sohan Rajpurohit <121212504+SohanRazz@users.noreply.github.com>
2023-01-18 16:42:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/LazyDeveloperr/LazyUrlHunterrBOT.git /LazyUrlHunterrBOT
-cd /LazyUrlHunterrBOT
+git clone https://github.com/SohanRazz/mdiskadavancebot
+cd /mdiskadavancebot
 pip3 install -U -r requirements.txt
 echo "⚡️⚡️ Starting LazyUrlHunterrBOT..."
 python3 main.py
