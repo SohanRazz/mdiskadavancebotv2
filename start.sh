08c29a1dda92c0cc0ad67b9725baa561a1804563
Sohan Rajpurohit <121212504+SohanRazz@users.noreply.github.com>
2023-01-19 01:39:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,9 +2,6 @@ if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
   git clone hhttps://github.com/SohanRazz/mdiskadavancebotv2 /mdiskadavancebotv2
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /mdiskadavancebotv2
 fi
 cd /mdiskadavancebotv2
 pip3 install -U -r requirements.txt
