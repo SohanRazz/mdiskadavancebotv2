eb35c9baf1845845ca74730820dff7e3444490ed
Sohan Rajpurohit <121212504+SohanRazz@users.noreply.github.com>
2023-01-18 22:57:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clonehttps://github.com/SohanRazz/mdiskadavancebot /mdiskadavancebot
+git clone https://github.com/SohanRazz/mdiskadavancebot.git /mdiskadavancebot
 cd /mdiskadavancebot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
