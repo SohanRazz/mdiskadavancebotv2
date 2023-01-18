78e023861a9e9a1c7bc84bf7a1a44c9d1defc50f
Sohan Rajpurohit <121212504+SohanRazz@users.noreply.github.com>
2023-01-19 01:36:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
-echo "Cloning Repo...."
-git clone https://github.com/SohanRazz/mdiskadavancebotv2.git /mdiskadavancebotv2
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone hhttps://github.com/SohanRazz/mdiskadavancebotv2 /mdiskadavancebotv2
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /mdiskadavancebotv2
+fi
 cd /mdiskadavancebotv2
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
