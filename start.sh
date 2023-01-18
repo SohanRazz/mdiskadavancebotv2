4c601f6ea608262dd700cd8d78d87ae0b158400f
Sohan Rajpurohit <121212504+SohanRazz@users.noreply.github.com>
2023-01-19 01:43:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,6 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone hhttps://github.com/SohanRazz/mdiskadavancebotv2 /mdiskadavancebotv2
-fi
+echo "Cloning Repo...."
+git clone https://github.com/SohanRazz/mdiskadavancebotv2.git /mdiskadavancebotv2
 cd /mdiskadavancebotv2
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "⚡️⚡️ Starting LazyUrlHunterrBOT..."
 python3 main.py
