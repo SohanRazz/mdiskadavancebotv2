62b2b3dacd316e5765aa1c88ca7019777e337403
Sohan Rajpurohit <121212504+SohanRazz@users.noreply.github.com>
2023-01-20 00:41:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@ git clone https://github.com/SohanRazz/mdiskadavancebotv2.git /mdiskadavancebotv
 cd /mdiskadavancebotv2
 pip freeze > requirements.txt
 pip3 install -U -r requirements.txt
+cd /mdiskadavancebotv2
 echo "Starting Bot...."
 python3 main.py
