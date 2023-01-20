c1fc094a6e24047151d7365a26184352da43e62e
Sohan Rajpurohit <121212504+SohanRazz@users.noreply.github.com>
2023-01-20 11:20:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Cyniteofficial/MdiskSearchBotV2.git /MdiskSearchBotV2
-cd /MdiskSearchBotV2
+git clone https://github.com/SohanRazz/mdiskadavancebotv2.git /mdiskadavancebotv2
+cd /mdiskadavancebotv2
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
