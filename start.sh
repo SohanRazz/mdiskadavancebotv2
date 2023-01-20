0358b1ea52f23e117d135c05e81da81fffea01df
Sohan Rajpurohit <121212504+SohanRazz@users.noreply.github.com>
2023-01-20 11:19:57 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-echo "Cloning Repo...."
-git clone https://github.com/SohanRazz/mdiskadavancebotv2.git /mdiskadavancebotv2
-cd /mdiskadavancebotv2
-pip3 install -U -r requirements.txt
-echo "⚡️⚡️ Starting LazyUrlHunterrBOT..."
-python3 main.py
